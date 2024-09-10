befc41c2f51aecdfaf76155b65c6b8017c123cbe
Victor Briaux <118478638+Malzware@users.noreply.github.com>
2024-09-10 12:23:01 +0200

    feat search engine Item

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -14,14 +14,13 @@ class ItemController extends Controller
 
     public function show(Item $item)
     {
-        return $item->load(['heldEvolutions', 'itemEvolutions']);
+        return $item->load([]);
     }
 
     public function search(Request $request)
     {
-        // Assurez-vous d'avoir configuré la recherche pour le modèle Item si nécessaire
         return Item::search($request->input('query'))
             ->get()
-            ->load(['heldEvolutions', 'itemEvolutions']);
+            ->load([]);
     }
 }
