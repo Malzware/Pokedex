e894176ec635827eb77a44bdf7403d55596c5896
Victor Briaux <118478638+Malzware@users.noreply.github.com>
2024-11-18 12:19:27 +0100

    feat modal moves

--- front-pokedex/src/app/pages/pokemon-abilities-moves/pokemon-abilities-moves.component.ts
+++ front-pokedex/src/app/pages/pokemon-abilities-moves/pokemon-abilities-moves.component.ts
@@ -10,4 +10,14 @@ import { Move } from "../../shared/interfaces/move";
 export class PokemonAbilitiesMovesComponent {
   @Input() abilities!: Ability[];
   @Input() moves!: Move[];
+
+  selectedMove: Move | null = null;  // Assurez-vous que selectedMove est soit null, soit un objet Move valide
+
+  openModal(move: Move): void {
+    this.selectedMove = move;  // Assigner le mouvement sélectionné
+    const modal = document.getElementById('my_modal_3') as HTMLDialogElement;
+    if (modal) {
+      modal.showModal();  // Ouvrir le modal
+    }
+  }
 }
--- front-pokedex/src/app/shared/interfaces/move.ts
+++ front-pokedex/src/app/shared/interfaces/move.ts
@@ -1,9 +1,20 @@
 export interface Move {
-    id: number;
-    name: string;
-    move_damage_class_id : number;
-    accuracy: number;
-    power: number;
-    pp: number;
-    priority: number;
-  }
+  id: number;
+  name: string;
+  move_damage_class_id: number;
+  accuracy: number;
+  power: number;
+  pp: number;
+  priority: number;
+  type_id: number;  // Le type du mouvement
+  created_at: string;  // Date de création du mouvement
+  updated_at: string;  // Date de dernière mise à jour du mouvement
+  description: string;  // Description du mouvement
+  pivot: {
+      pokemon_variety_id: number;  // ID de la variété de Pokémon
+      move_id: number;  // ID du mouvement
+      move_learn_method_id: number;  // Méthode d'apprentissage du mouvement
+      game_version_id: number;  // Version du jeu
+      level: number;  // Niveau d'apprentissage du mouvement
+  };
+}
