2fc4a29aa5a67f000e92a689f1aa04b5b8276db2
Victor Briaux <118478638+Malzware@users.noreply.github.com>
2024-12-06 14:31:40 +0100

    fix migration name

--- back-pokedex/config/services.php
+++ back-pokedex/config/services.php
@@ -35,12 +35,6 @@ return [
         ],
     ],
 
-    'google' => [
-        'client_id' => env('GOOGLE_CLIENT_ID'),
-        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => env('GOOGLE_REDIRECT_URI'),
-    ],
-
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
