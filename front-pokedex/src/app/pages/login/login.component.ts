f633db205f58b105dba9ce496f97f4ea7aea5cca
Victor Briaux <118478638+Malzware@users.noreply.github.com>
2024-12-21 17:30:54 +0100

    style login css

--- front-pokedex/src/app/pages/login/login.component.ts
+++ front-pokedex/src/app/pages/login/login.component.ts
@@ -4,18 +4,24 @@ import { ApiService } from '../../shared/services/api.service';
 @Component({
   selector: 'app-login',
   template: `
-    <div class="flex min-h-screen items-center justify-center">
-      <button
-        class="btn btn-primary"
+    <div class="flex flex-col items-center justify-center min-h-screen">
+      <img 
+        src="assets/image/pokeball_pokedex.png" 
+        alt="Pokédex" 
+      />
+      <h1 class="text-5xl font-bold text-gray-800 mb-8">Pokédex</h1>
+      <button 
+        class="bg-gray-300 text-gray-700 py-3 px-8 rounded-full text-lg font-semibold hover:bg-gray-400 transition"
         (click)="login()"
       >
         Se connecter avec GitHub
       </button>
     </div>
-  `
+  `,
+  styleUrls: []
 })
 export class LoginComponent {
-  constructor(private apiService: ApiService) {}
+  constructor(private apiService: ApiService) { }
 
   async login() {
     await this.apiService.redirectToGithub();
